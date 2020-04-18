ef56c71cc5c2d59f35bf30e3dc7cd81dc4961e1f
Yan-Zhengchi <302565905@qq.com>
2020-04-18 11:40:23 +0800

    用idea集成的git向远程仓库中添加文件

---
+++ gitTest/src/GitTest.java
@@ -0,0 +1,19 @@
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssss");
+    }
+}
